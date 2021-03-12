2718bf82516de4e802f4d679743f196c3883752c
FYUR1K <55157891+FYUR1K@users.noreply.github.com>
2021-03-12 20:48:28 +0300

    Update Region.h

--- Region.h
+++ Region.h
@@ -1,16 +1,18 @@
 #ifndef Region_H
 #define Region_H
 
-class Region(){
+class Region{
 public:
-Region(const std::string &name_, const float &payments_, const float &min_average_income_);
+    Region();
+    Region(const std::string &NAME, const float &AMOUNT, const float &THRESHHOLD);
+    void print();
 
-
-void print_Region_param(const std::string &name, const float &payments, const float &min_average_income );
 protected:
     std::string name;
-    float payments = 0;
-    float min_average_income = 0;
+    float amount_of_payment_per_family = 0;
+    float threshold = 0;
+
+
 };
 
 
