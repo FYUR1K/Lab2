7949c08b43fca211354336bbba9d04b2ad7e474a
FYUR1K <55157891+FYUR1K@users.noreply.github.com>
2021-03-23 14:19:18 +0300

    Update Family_from_region.cpp

--- Family_from_region.cpp
+++ Family_from_region.cpp
@@ -3,9 +3,9 @@
 float Family_from_region::average_income(){
     if((total_income / count_of_people) < threshold){
         total_income += amount_of_payment_per_family;
-        return total_income/count_of_people;
+        return Family::average_income();
     } else{
-        return total_income / count_of_people;
+        return Family::average_income();
     }
 }
 
@@ -13,6 +13,8 @@ void Family_from_region::print(){
     Family::print();
     Region::print();
 }
+
+
 
 void Family_from_region::setr1(const std::string &SURNAME, const size_t &COUNT, const float &income,const std::string &NAME,
                                const float &AMOUNT, const float &THRESHHOLD){
