b8521f79ec852fde1962ac782899677c8b9d131a
FYUR1K <55157891+FYUR1K@users.noreply.github.com>
2021-03-23 14:19:45 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,19 @@
-#include<iostream>
-#include"Family.cpp"
-#include"Region.cpp"
-#include"Family_from_region.cpp"
+#include"Family_from_region.h"
+
 
 int main(){
-Family Griffins;
+    Family Griffins;
     Griffins.setr("Griffins", 4, 20000);
     Griffins.print();
 
-Region Moscow("Moscow", 4000, 5000);
+    Region Moscow("Moscow", 4000, 5000);
     Moscow.print();
 
 
-Family_from_region Griffins_from_Moscow;
+    Family_from_region Griffins_from_Moscow;
     Griffins_from_Moscow.setr1("Griffins", 4, 19000,"Moscow", 4000, 5000);
     std::cout << Griffins_from_Moscow.average_income() << '\n';
     Griffins_from_Moscow.print();
 
-return 0;
+    return 0;
 }
