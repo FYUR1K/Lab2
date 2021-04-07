e4f9277c0123319d6f5b64f9de1090fbc2b874f9
FYUR1K <55157891+FYUR1K@users.noreply.github.com>
2021-04-07 17:17:40 +0300

    Update Family_from_region.cpp

--- Family_from_region.cpp
+++ Family_from_region.cpp
@@ -1,8 +1,15 @@
 #include"Family_from_region.h"
 
+
+Family_from_region::Family_from_region(): Family(), Region(){}
+
+Family_from_region::Family_from_region(const std::string &SURNAME, const size_t &COUNT, const float &income,const std::string &NAME,
+                                       const float &AMOUNT, const float &THRESHHOLD): Family(SURNAME,COUNT,income), Region(NAME,AMOUNT,THRESHHOLD){}
+
 float Family_from_region::average_income(){
-    if((total_income / count_of_people) < threshold){
-        total_income += amount_of_payment_per_family;
+    if(((total_income[0] / count_of_people) < threshold) && total_income[1] !=1){
+        total_income[0] += amount_of_payment_per_family;
+        total_income.push_back(1);
         return Family::average_income();
     } else{
         return Family::average_income();
@@ -13,19 +20,3 @@ void Family_from_region::print(){
     Family::print();
     Region::print();
 }
-
-
-
-void Family_from_region::setr1(const std::string &SURNAME, const size_t &COUNT, const float &income,const std::string &NAME,
-                               const float &AMOUNT, const float &THRESHHOLD){
-    surname = SURNAME;
-    count_of_people = COUNT;
-    total_income = income;
-    name = NAME;
-    amount_of_payment_per_family = AMOUNT;
-    threshold = THRESHHOLD;
-}
-
-
-
-
