7ea070c7b302cb80ea63fec2d17632b8a7079ef1
FYUR1K <55157891+FYUR1K@users.noreply.github.com>
2021-04-07 17:16:32 +0300

    Update Family.cpp

--- Family.cpp
+++ Family.cpp
@@ -1,17 +1,22 @@
 #include "Family.h"
 
-float Family::average_income() {
-    return total_income / count_of_people;
+Family::Family(): surname(""),count_of_people(0),
+ total_income(){
 }
 
-void Family::setr(const std::string &SURNAME, const size_t &COUNT, const float &income ){
-    surname = SURNAME;
-    count_of_people = COUNT;
-    total_income = income;
+Family::Family(const std::string &SURNAME, const size_t &COUNT, const float &income):surname(SURNAME),
+    count_of_people(COUNT)
+
+{total_income.push_back(income);}
+
+float Family::average_income() {
+    return total_income[0] / count_of_people;
 }
+
+
 void Family::print(){
     std::cout << "Фамилия: " << surname << '\n';
     std::cout << "Число человек: " << count_of_people << '\n';
-    std::cout << "Сумарный доход семьи в месяц: " << total_income << '\n' << '\n';
+    std::cout << "Сумарный доход семьи в месяц: " << total_income[0] << '\n' << '\n';
 }
 
